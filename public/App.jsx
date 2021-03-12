fae119f50e5ac62ab8cb2e3d677a44413f2ff76e
sadnessOjisan <sadness.ojisan@gmail.com>
2021-03-13 01:59:22 +0900

    can gen counter

---
+++ public/App.jsx
@@ -0,0 +1,304 @@
+const App = () => {
+const [count, setCount] = useState(0)
+return <Component1 onClick={()=>setCount(count + 1)} />
+}
+const Component1 = (props) => {
+return <Component2 onClick={props.onClick} />
+}
+const Component2 = (props) => {
+return <Component3 onClick={props.onClick} />
+}
+const Component3 = (props) => {
+return <Component4 onClick={props.onClick} />
+}
+const Component4 = (props) => {
+return <Component5 onClick={props.onClick} />
+}
+const Component5 = (props) => {
+return <Component6 onClick={props.onClick} />
+}
+const Component6 = (props) => {
+return <Component7 onClick={props.onClick} />
+}
+const Component7 = (props) => {
+return <Component8 onClick={props.onClick} />
+}
+const Component8 = (props) => {
+return <Component9 onClick={props.onClick} />
+}
+const Component9 = (props) => {
+return <Component10 onClick={props.onClick} />
+}
+const Component10 = (props) => {
+return <Component11 onClick={props.onClick} />
+}
+const Component11 = (props) => {
+return <Component12 onClick={props.onClick} />
+}
+const Component12 = (props) => {
+return <Component13 onClick={props.onClick} />
+}
+const Component13 = (props) => {
+return <Component14 onClick={props.onClick} />
+}
+const Component14 = (props) => {
+return <Component15 onClick={props.onClick} />
+}
+const Component15 = (props) => {
+return <Component16 onClick={props.onClick} />
+}
+const Component16 = (props) => {
+return <Component17 onClick={props.onClick} />
+}
+const Component17 = (props) => {
+return <Component18 onClick={props.onClick} />
+}
+const Component18 = (props) => {
+return <Component19 onClick={props.onClick} />
+}
+const Component19 = (props) => {
+return <Component20 onClick={props.onClick} />
+}
+const Component20 = (props) => {
+return <Component21 onClick={props.onClick} />
+}
+const Component21 = (props) => {
+return <Component22 onClick={props.onClick} />
+}
+const Component22 = (props) => {
+return <Component23 onClick={props.onClick} />
+}
+const Component23 = (props) => {
+return <Component24 onClick={props.onClick} />
+}
+const Component24 = (props) => {
+return <Component25 onClick={props.onClick} />
+}
+const Component25 = (props) => {
+return <Component26 onClick={props.onClick} />
+}
+const Component26 = (props) => {
+return <Component27 onClick={props.onClick} />
+}
+const Component27 = (props) => {
+return <Component28 onClick={props.onClick} />
+}
+const Component28 = (props) => {
+return <Component29 onClick={props.onClick} />
+}
+const Component29 = (props) => {
+return <Component30 onClick={props.onClick} />
+}
+const Component30 = (props) => {
+return <Component31 onClick={props.onClick} />
+}
+const Component31 = (props) => {
+return <Component32 onClick={props.onClick} />
+}
+const Component32 = (props) => {
+return <Component33 onClick={props.onClick} />
+}
+const Component33 = (props) => {
+return <Component34 onClick={props.onClick} />
+}
+const Component34 = (props) => {
+return <Component35 onClick={props.onClick} />
+}
+const Component35 = (props) => {
+return <Component36 onClick={props.onClick} />
+}
+const Component36 = (props) => {
+return <Component37 onClick={props.onClick} />
+}
+const Component37 = (props) => {
+return <Component38 onClick={props.onClick} />
+}
+const Component38 = (props) => {
+return <Component39 onClick={props.onClick} />
+}
+const Component39 = (props) => {
+return <Component40 onClick={props.onClick} />
+}
+const Component40 = (props) => {
+return <Component41 onClick={props.onClick} />
+}
+const Component41 = (props) => {
+return <Component42 onClick={props.onClick} />
+}
+const Component42 = (props) => {
+return <Component43 onClick={props.onClick} />
+}
+const Component43 = (props) => {
+return <Component44 onClick={props.onClick} />
+}
+const Component44 = (props) => {
+return <Component45 onClick={props.onClick} />
+}
+const Component45 = (props) => {
+return <Component46 onClick={props.onClick} />
+}
+const Component46 = (props) => {
+return <Component47 onClick={props.onClick} />
+}
+const Component47 = (props) => {
+return <Component48 onClick={props.onClick} />
+}
+const Component48 = (props) => {
+return <Component49 onClick={props.onClick} />
+}
+const Component49 = (props) => {
+return <Component50 onClick={props.onClick} />
+}
+const Component50 = (props) => {
+return <Component51 onClick={props.onClick} />
+}
+const Component51 = (props) => {
+return <Component52 onClick={props.onClick} />
+}
+const Component52 = (props) => {
+return <Component53 onClick={props.onClick} />
+}
+const Component53 = (props) => {
+return <Component54 onClick={props.onClick} />
+}
+const Component54 = (props) => {
+return <Component55 onClick={props.onClick} />
+}
+const Component55 = (props) => {
+return <Component56 onClick={props.onClick} />
+}
+const Component56 = (props) => {
+return <Component57 onClick={props.onClick} />
+}
+const Component57 = (props) => {
+return <Component58 onClick={props.onClick} />
+}
+const Component58 = (props) => {
+return <Component59 onClick={props.onClick} />
+}
+const Component59 = (props) => {
+return <Component60 onClick={props.onClick} />
+}
+const Component60 = (props) => {
+return <Component61 onClick={props.onClick} />
+}
+const Component61 = (props) => {
+return <Component62 onClick={props.onClick} />
+}
+const Component62 = (props) => {
+return <Component63 onClick={props.onClick} />
+}
+const Component63 = (props) => {
+return <Component64 onClick={props.onClick} />
+}
+const Component64 = (props) => {
+return <Component65 onClick={props.onClick} />
+}
+const Component65 = (props) => {
+return <Component66 onClick={props.onClick} />
+}
+const Component66 = (props) => {
+return <Component67 onClick={props.onClick} />
+}
+const Component67 = (props) => {
+return <Component68 onClick={props.onClick} />
+}
+const Component68 = (props) => {
+return <Component69 onClick={props.onClick} />
+}
+const Component69 = (props) => {
+return <Component70 onClick={props.onClick} />
+}
+const Component70 = (props) => {
+return <Component71 onClick={props.onClick} />
+}
+const Component71 = (props) => {
+return <Component72 onClick={props.onClick} />
+}
+const Component72 = (props) => {
+return <Component73 onClick={props.onClick} />
+}
+const Component73 = (props) => {
+return <Component74 onClick={props.onClick} />
+}
+const Component74 = (props) => {
+return <Component75 onClick={props.onClick} />
+}
+const Component75 = (props) => {
+return <Component76 onClick={props.onClick} />
+}
+const Component76 = (props) => {
+return <Component77 onClick={props.onClick} />
+}
+const Component77 = (props) => {
+return <Component78 onClick={props.onClick} />
+}
+const Component78 = (props) => {
+return <Component79 onClick={props.onClick} />
+}
+const Component79 = (props) => {
+return <Component80 onClick={props.onClick} />
+}
+const Component80 = (props) => {
+return <Component81 onClick={props.onClick} />
+}
+const Component81 = (props) => {
+return <Component82 onClick={props.onClick} />
+}
+const Component82 = (props) => {
+return <Component83 onClick={props.onClick} />
+}
+const Component83 = (props) => {
+return <Component84 onClick={props.onClick} />
+}
+const Component84 = (props) => {
+return <Component85 onClick={props.onClick} />
+}
+const Component85 = (props) => {
+return <Component86 onClick={props.onClick} />
+}
+const Component86 = (props) => {
+return <Component87 onClick={props.onClick} />
+}
+const Component87 = (props) => {
+return <Component88 onClick={props.onClick} />
+}
+const Component88 = (props) => {
+return <Component89 onClick={props.onClick} />
+}
+const Component89 = (props) => {
+return <Component90 onClick={props.onClick} />
+}
+const Component90 = (props) => {
+return <Component91 onClick={props.onClick} />
+}
+const Component91 = (props) => {
+return <Component92 onClick={props.onClick} />
+}
+const Component92 = (props) => {
+return <Component93 onClick={props.onClick} />
+}
+const Component93 = (props) => {
+return <Component94 onClick={props.onClick} />
+}
+const Component94 = (props) => {
+return <Component95 onClick={props.onClick} />
+}
+const Component95 = (props) => {
+return <Component96 onClick={props.onClick} />
+}
+const Component96 = (props) => {
+return <Component97 onClick={props.onClick} />
+}
+const Component97 = (props) => {
+return <Component98 onClick={props.onClick} />
+}
+const Component98 = (props) => {
+return <Component99 onClick={props.onClick} />
+}
+const Component99 = (props) => {
+return <Component100 onClick={props.onClick} />
+}
+const Component100 = (props) => {
+return <button onClick={props.onClick}>click me</button>
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
 
 fn main() {
-    let mut file = File::create("foo.txt").unwrap();
-    writeln!(&mut file, "Hello World!").unwrap();
+    let mut file = File::create("public/App.jsx").unwrap();
+    writeln!(&mut file, "const App = () => {{").unwrap();
+    writeln!(&mut file, "const [count, setCount] = useState(0)").unwrap();
+    writeln!(&mut file, "return <><Component1 onClick={{()=>setCount(count + 1)}} />{{count}}</>").unwrap();
+    writeln!(&mut file, "}}").unwrap();
+    let end = 101;
+    for n in 1..end {
+        let code = format!("const Component{} = (props) => {{", n);
+        writeln!(&mut file, "{}", code).unwrap();
+        
+        if(n == end-1){
+            writeln!(&mut file, "return <button onClick={{props.onClick}}>click me</button>").unwrap();
+
+        writeln!(&mut file, "}}").unwrap();
+        }else{
+            let call = format!("return <Component{} onClick={{props.onClick}} />", n+1);
+            writeln!(&mut file, "{}", call).unwrap();
+            writeln!(&mut file, "}}").unwrap();
+        }
+        }
 }
